6bc5d2eaacb25a92b49487430d3a914d691e24dd
takahiro-sekine <doctype.html.1021@gmail.com>
2020-11-02 16:47:49 +0900

    Update 20201029024313_create_prototypes.rb

--- db/migrate/20201029024313_create_prototypes.rb
+++ db/migrate/20201029024313_create_prototypes.rb
@@ -4,7 +4,6 @@ class CreatePrototypes < ActiveRecord::Migration[6.0]
       t.string :title
       t.text :catch_copy
       t.text :concept
-      t.references :image, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps
     end
